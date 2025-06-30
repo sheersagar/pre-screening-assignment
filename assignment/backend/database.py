351978c4e0b6bf5c3e6542af7bdf715313f9aa70
vishav deshwal <vishavdeshwal@gmail.com>
2025-06-30 21:49:46 +0530

    corrected the path of databse url in database.py code

--- assignment/backend/database.py
+++ assignment/backend/database.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_db_connection():
     return psycopg2.connect(DATABASE_URL)
