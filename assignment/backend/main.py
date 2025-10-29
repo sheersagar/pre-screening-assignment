dff6c3547a632901a62b226d1e6848380b65c433
vishav deshwal <vishavdeshwal@gmail.com>
2025-10-29 22:21:38 +0530

    edited Docker file backend, main.py backend, /backend/.env

--- assignment/backend/main.py
+++ assignment/backend/main.py
@@ -15,7 +15,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["frontend_origin"],  # Adjust this to your frontend origin
+    allow_origins=[frontend_origin],  # Adjust this to your frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
