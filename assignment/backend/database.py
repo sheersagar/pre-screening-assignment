80ed78174be1cea61fdb904a349c29a08fc46da9
vishav deshwal <vishavdeshwal@gmail.com>
2025-11-02 22:22:31 +0530

    edited Docker file of frontend with Node:18 and added appliaction yaml file

--- assignment/backend/database.py
+++ assignment/backend/database.py
@@ -3,9 +3,11 @@ from psycopg2 import sql
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# load_dotenv(). # Only required when deploying locally and not in cluster
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_db_connection():
+    if not DATABASE_URL:
+        raise ValueError("DATABASE_URL environment variable is not set")
     return psycopg2.connect(DATABASE_URL)
